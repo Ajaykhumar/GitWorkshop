b01826bde4cb0f9fb18ae95c6b0f44d857ca6e5a
Ajay <gvajaykhumarvelmurugan@gmail.com>
2023-09-19 11:24:07 -0700

    version 1|Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args){
+        System.out.println(x:"Hello");
+        System.out.println(x:"Hey");
+        System.out.println(x:"everyone");
+
+        }
+}
